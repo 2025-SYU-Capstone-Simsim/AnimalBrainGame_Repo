59e15d3f2a640ae6c75364b947e4d07bedc2f5aa
ryul00 <gju07059@gmail.com>
2025-05-06 17:44:44 +0900

    희영게임2개 추가

--- assets/scripts/Controller/SingleGameList.ts
+++ assets/scripts/Controller/SingleGameList.ts
@@ -12,6 +12,17 @@ export default class MainController extends cc.Component {
         console.log("쓰리매치 게임으로 이동");
         cc.director.loadScene('3M_GameScene');
     }
+
+    loadBlockCountGame() {
+        console.log("블록 개수 세기 게임으로 이동");
+        const result = cc.director.loadScene('BlockCountGameScene');
+        console.log("씬 로드 결과:", result);
+    }
+
+    loadRememberGame() {
+        console.log("기억력 게임으로 이동");
+        cc.director.loadScene('RememberGameScene');
+    }
     
     
  // 추후 실제 씬이름 추가 , 코코스 내 빌드파일에도 추가
