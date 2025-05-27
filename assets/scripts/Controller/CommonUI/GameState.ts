1122943bbb2c1e6efb87d9bb5e7bbafd1abbead8
ryul00 <gju07059@gmail.com>
2025-05-27 13:29:47 +0900

    멀티 방입장 -> 멀티 게임리스트 연결 성공

--- assets/scripts/Controller/CommonUI/GameState.ts
+++ assets/scripts/Controller/CommonUI/GameState.ts
@@ -5,7 +5,11 @@ export default class GameState {
   static score: number = 0;
   static gameId: string = '';
   static incomingRoomId: string = '';
-  static isHost: boolean = false; // 역할 구분 추가
+  static createdRoomId: string = ''; // 스트용 방 ID 저장
+  static isHost: boolean = false;
+
+  //   incomingRoomId : 초대받은 방 ID (게스트용) ->	http://...?roomId=XYZ123
+//   createdRoomId : 생성한 방 ID (호스트용) 생성 -> API 응답 후 저장
 
   static recentSingleScores: {
     gameId: string;
@@ -23,6 +27,9 @@ export default class GameState {
     this.character = '';
     this.recentSingleScores = [];
     this.incomingRoomId = '';
-    // this.isHost = false;
+    this.createdRoomId = ''; // 초기화 시 함께 리셋
+    this.isHost = false; 
   }
+
+
 }
