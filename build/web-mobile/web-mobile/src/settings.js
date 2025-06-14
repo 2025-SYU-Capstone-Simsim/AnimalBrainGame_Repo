a536fc9ed4a6fc916b4aaf7077db31a194165c5b
ryul00 <gju07059@gmail.com>
2025-06-15 01:53:55 +0900

    최최최종

--- build/web-mobile/web-mobile/src/settings.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Common/CommonUI/SplashMain.fire",orientation:"",jsList:["assets/sockets/socket.io.min.js"]};
