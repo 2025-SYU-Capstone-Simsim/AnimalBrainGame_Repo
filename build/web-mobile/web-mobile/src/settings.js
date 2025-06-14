e2b1e4ee264c06af8d787c239352d7e091ca01ee
ryul00 <gju07059@gmail.com>
2025-06-15 01:41:36 +0900

    다시 빌드

---
+++ build/web-mobile/web-mobile/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Common/CommonUI/SplashMain.fire",orientation:"",jsList:["assets/sockets/socket.io.min.js"]};
