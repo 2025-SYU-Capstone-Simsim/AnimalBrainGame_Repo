75fbb8b4779f6ce3a3d1c3f620037eb306157124
ryul00 <gju07059@gmail.com>
2025-06-15 23:35:21 +0900

    마이페이지 최종 수정

--- assets/scripts/Controller/MyPage/MyInfoController.ts
+++ assets/scripts/Controller/MyPage/MyInfoController.ts
@@ -4,43 +4,51 @@ const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class MyPage extends cc.Component {
-  @property(cc.Sprite)
-  characterSprite: cc.Sprite = null;
+    @property(cc.Sprite)
+    characterSprite: cc.Sprite = null;
 
-  @property(cc.Label)
-  nicknameLabel: cc.Label = null;
+    @property(cc.Label)
+    nicknameLabel: cc.Label = null;
 
-   @property(cc.Button)
-  deleteAccountButton: cc.Button = null;
+    @property(cc.Button)
+    deleteAccountButton: cc.Button = null;
 
-  start() {
-    this.nicknameLabel.string = GameState.nickname || "이름 없음";
-    this.setCharacterSprite(this.characterSprite.node, GameState.character);
-  }
+    @property(cc.Button)
+    BackButton: cc.Button = null;
 
-  setCharacterSprite(node: cc.Node, characterKey: string) {
-    const sprite = node.getComponent(cc.Sprite);
-    if (!sprite) return;
-
-    if (!['dog', 'rabbit', 'tiger'].includes(characterKey)) {
-      characterKey = 'dog';
+    start() {
+        this.nicknameLabel.string = GameState.nickname || "이름 없음";
+        this.setCharacterSprite(this.characterSprite.node, GameState.character);
     }
 
-    const path = `Images/Common/characters/${characterKey}`;
-    cc.resources.load(path, cc.SpriteFrame, (err, spriteFrame) => {
-      if (!err && spriteFrame) {
-        sprite.spriteFrame = spriteFrame;
-      }
-    });
-  }
+    setCharacterSprite(node: cc.Node, characterKey: string) {
+        const sprite = node.getComponent(cc.Sprite);
+        if (!sprite) return;
 
-  onClickDeleteAccount() {
-    localStorage.clear(); // 저장된 토큰, 닉네임, 캐릭터 등 삭제
-    GameState.nickname = '';
-    GameState.character = '';
-    GameState.browserId = '';
+        if (!['dog', 'rabbit', 'tiger'].includes(characterKey)) {
+            characterKey = 'dog';
+        }
+
+        const path = `Images/Common/characters/${characterKey}`;
+        cc.resources.load(path, cc.SpriteFrame, (err, spriteFrame) => {
+            if (!err && spriteFrame) {
+                sprite.spriteFrame = spriteFrame;
+            }
+        });
+    }
+
+    onClickDeleteAccount() {
+        localStorage.clear(); // 저장된 토큰, 닉네임, 캐릭터 등 삭제
+        GameState.nickname = '';
+        GameState.character = '';
+        GameState.browserId = '';
 
 
-    cc.director.loadScene("LoginScene"); // 로그인 씬으로 이동
-  }
+        cc.director.loadScene("LoginScene"); // 로그인 씬으로 이동
+    }
+
+    onClickMain() {
+        cc.log("뒤로가기 버튼 클릭됨. Main 씬으로 이동.");
+        cc.director.loadScene("MainScene");
+    }
 }
